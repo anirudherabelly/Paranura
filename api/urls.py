19478f4d6b8725cc5f534bfaf6209ece687fbfd2
anirudherabelly <anirudh.erabelly@gmail.com>
2020-06-02 17:04:13 -0500

    part1 and part2 done

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('company/<str:name>/', views.findAllEmployeesInCompany, name='findAllEmployeesInCompany'),
+    path('friends', views.findCommonFriends, name='findCommonFriends'),
+]
